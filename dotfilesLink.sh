e0b2a5053f0b0611729db98dee7e439495ea691c
Toshiki <toshiki.k.000@gmail.com>
2018-06-26 11:23:38 +0900

    Move a tmux file

--- dotfilesLink.sh
+++ dotfilesLink.sh
@@ -3,4 +3,5 @@ ln -sf ~/dotfiles/zsh/.zshenv ~/.zshenv
 ln -sf ~/dotfiles/zsh/.zlogin ~/.zlogin
 ln -sf ~/dotfiles/zsh/.zshrc ~/.zshrc
 ln -sf ~/dotfiles/vim/.vimrc ~/.vimrc
+ln -sf ~/dotfiles/tmux/.tmux.conf ~/.tmux.conf
 
