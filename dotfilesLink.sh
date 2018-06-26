6ac64e15e563e20adcbd5a7919e0247b18f6dae9
Toshiki <toshiki.k.000@gmail.com>
2018-06-26 11:08:37 +0900

    Move a vim file

--- dotfilesLink.sh
+++ dotfilesLink.sh
@@ -2,5 +2,5 @@
 ln -sf ~/dotfiles/.zshenv ~/.zshenv
 ln -sf ~/dotfiles/.zsh_profile ~/.zsh_profile
 ln -sf ~/dotfiles/.zshrc ~/.zshrc
-ln -sf ~/dotfiles/.vimrc ~/.vimrc
+ln -sf ~/dotfiles/vim/.vimrc ~/.vimrc
 
