d15d80d5bcfa92589cbe5c8e456fcc6d31cb9fc1
Toshiki <toshiki.k.000@gmail.com>
2018-06-26 11:19:08 +0900

    Move zsh files

--- dotfilesLink.sh
+++ dotfilesLink.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-ln -sf ~/dotfiles/.zshenv ~/.zshenv
-ln -sf ~/dotfiles/.zsh_profile ~/.zsh_profile
-ln -sf ~/dotfiles/.zshrc ~/.zshrc
+ln -sf ~/dotfiles/zsh/.zshenv ~/.zshenv
+ln -sf ~/dotfiles/zsh/.zlogin ~/.zlogin
+ln -sf ~/dotfiles/zsh/.zshrc ~/.zshrc
 ln -sf ~/dotfiles/vim/.vimrc ~/.vimrc
 
