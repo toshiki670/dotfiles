1c3d5282d09cb9b4ec39607fe57f28be7c7582f9
Toshiki <toshiki.k.000@gmail.com>
2018-07-19 01:17:21 +0900

    Add_bash_files

---
+++ bash/.bashrc
@@ -0,0 +1,6 @@
+PS1="\[\e[0;36m\]\u@\H \W\[\e[0m\]\$ "
+
+alias ll='ls -l'
+alias la='ls -a'
+alias lla='ls -la'
+
---
+++ bash/install.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+
+ln -sf ~/dotfiles/bash/.bash_profile ~/.bash_profile
+ln -sf ~/dotfiles/bash/.bashrc ~/.bashrc
+
