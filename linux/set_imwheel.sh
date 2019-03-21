5cc7949769365594ad9b1caaeffeb5be43d27154
Toshiki <toshiki.k.000@gmail.com>
2019-03-21 21:14:16 +0900

    Update: imwheel setup file

--- linux/set_imwheel.sh
+++ linux/set_imwheel.sh
@@ -1,5 +1,15 @@
 #!/bin/bash
 
-ln -sf ./imwheel.service /etc/systemd/system/imwheel.service 
+service=imwheel.service
+users_systemd=~/.config/systemd/user
 
+if [ ! -e $users_systemd ]; then
+  mkdir -p $users_systemd
+  echo "Made path that user's systemd"
+fi
+
+cp ./$service $users_systemd/$service
+
+
+[ $? -eq "0" ] && echo "Success!"
 
