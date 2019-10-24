d05e49b79625d7b9f9d99112cefd412ee504942d
Toshiki <toshiki.k.000@gmail.com>
2019-10-24 23:04:47 +0900

    Change to make folder

--- dotfilesLink.sh
+++ dotfilesLink.sh
@@ -1,7 +1,14 @@
 #!/bin/bash
+
+# Zsh Setup
 ln -sf ~/dotfiles/zsh/.zshenv ~/.zshenv
 ln -sf ~/dotfiles/zsh/.zlogin ~/.zlogin
 ln -sf ~/dotfiles/zsh/.zshrc ~/.zshrc
+
+# Vim Setup
+mkdir -p ~/.config/nvim/
 ln -sf ~/dotfiles/vim/.vimrc ~/.config/nvim/init.vim
+
+# Tmux Setup
 ln -sf ~/dotfiles/tmux/.tmux.conf ~/.tmux.conf
 
