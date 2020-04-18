6b4ac9282d79c4a4b89fb68c2b4c255d61fe5a68
Toshiki <toshiki.dev@protonmail.ch>
2020-04-18 12:38:38 +0900

    Add echo 'master'

--- git/install.sh
+++ git/install.sh
@@ -1,4 +1,9 @@
+echo -n 'Writing ...'
 {
+  echo "### Dotfile's config ###"
   echo -e "[include]\n  path = ~/dotfiles/git/git.config\n"
   echo -e "[include]\n  path = ~/dotfiles/git/user.config\n"
+  echo "### End ###"
 } >> ~/.gitconfig
+
+echo ' ok'
