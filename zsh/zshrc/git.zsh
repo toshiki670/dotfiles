ec81abd8d5b10e1f69ea8051806b53084bdafabc
Toshiki <toshiki.dev@protonmail.ch>
2020-09-12 00:06:19 +0900

    Set git-flow command alias for ubuntu 'feature/#45'

--- zsh/zshrc/git.zsh
+++ zsh/zshrc/git.zsh
@@ -13,10 +13,24 @@ alias gpush='git push'
 alias glog="git log --graph --pretty=format:'%C(yellow)%h%C(cyan)%d%Creset %s %C(white)- %an, %ar%Creset'"
 alias g-reset-hard='git reset --hard HEAD'
 
+
 # Git flow
 # yay -S gitflow-avh
 # apt install git-flow
+readonly TRUE=1
+readonly FALSE=0
+
+git_flow_exists=$FALSE
+
 if type "git-flow" > /dev/null 2>&1; then
+  # for Arch Linux
+  git_flow_exists=$TRUE
+elif dpkg -l | grep git-flow > /dev/null 2>&1; then
+  # for Ubuntu
+  git_flow_exists=$TRUE
+fi
+
+if [[ $git_flow_exists == $TRUE ]]; then
   alias @='git-flow' # deprecated for next version
   alias @feature='git-flow feature'
   alias @hotfix='git-flow hotfix'
