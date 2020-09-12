c0504b1b0553c948693370600038482e9f421437
Toshiki <toshiki.dev@protonmail.ch>
2020-09-12 21:29:04 +0900

    Add Installer

---
+++ powershell/install.bat
@@ -0,0 +1,19 @@
+@echo off
+
+rem Exist check
+where pwsh 2> nul > nul
+if not %errorlevel% == 0 (
+    echo Not found: PowerShell Core
+    pause
+    exit
+)
+
+rem Get PowerShell profile location
+for /F %%i in ('pwsh -c echo $profile') do set PWSH_PROFILE_PATH=%%i
+
+rem Make simbolic link
+mklink %PWSH_PROFILE_PATH% %USERPROFILE%\dotfiles\powershell\Microsoft.PowerShell_profile.ps1
+
+echo Success!
+
+pause
