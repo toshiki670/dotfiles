82f2e6921386477f4d918f36c2510303841ca401
Toshiki <toshiki.dev@protonmail.ch>
2020-09-12 21:28:01 +0900

    Add PowerShell profile

---
+++ powershell/Microsoft.PowerShell_profile.ps1
@@ -0,0 +1,11 @@
+# Pure-pwsh
+Import-Module pure-pwsh
+Import-Module posh-git
+
+# For git
+Set-Alias g 'git'
+Set-Alias gad 'git add'
+Set-Alias gap 'git add -p'
+Set-Alias gd 'git diff'
+Set-Alias gds 'git diff --staged'
+Set-Alias gs 'git status'
