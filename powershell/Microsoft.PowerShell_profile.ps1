c33205b1d79ab37a2be2c6b84393b21695f81fe7
Toshiki <toshiki.dev@protonmail.ch>
2020-09-21 15:08:12 +0900

    Fix git alias command

--- powershell/Microsoft.PowerShell_profile.ps1
+++ powershell/Microsoft.PowerShell_profile.ps1
@@ -3,9 +3,25 @@ Import-Module pure-pwsh
 Import-Module posh-git
 
 # For git
-Set-Alias g 'git'
-Set-Alias gad 'git add'
-Set-Alias gap 'git add -p'
-Set-Alias gd 'git diff'
-Set-Alias gds 'git diff --staged'
-Set-Alias gs 'git status'
+function Git-Custom-Add-Stage($Path) {
+    git add $Path
+}
+function Git-Custom-Add-Stage-by-Patch {
+    git add -p
+}
+function Git-Custom-Show-Diff {
+    git diff
+}
+function Git-Custom-Show-Diff-On-Staged {
+    git diff --staged
+}
+function Git-Custom-Show-Status {
+    git status
+}
+
+Set-Alias g git
+Set-Alias gad Git-Custom-Add-Stage
+Set-Alias gap Git-Custom-Add-Stage-by-Patch
+Set-Alias gd Git-Custom-Show-Diff
+Set-Alias gds Git-Custom-Show-Diff-On-Staged
+Set-Alias gs Git-Custom-Show-Status
