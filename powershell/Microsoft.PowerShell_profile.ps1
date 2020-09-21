56fa158b45c391da886b199cf08d227d84ccbd02
Toshiki <toshiki.dev@protonmail.ch>
2020-09-21 16:32:43 +0900

    Add git flow alias #58

--- powershell/Microsoft.PowerShell_profile.ps1
+++ powershell/Microsoft.PowerShell_profile.ps1
@@ -25,3 +25,31 @@ Set-Alias gap Git-Custom-Add-Stage-by-Patch
 Set-Alias gd Git-Custom-Show-Diff
 Set-Alias gds Git-Custom-Show-Diff-On-Staged
 Set-Alias gs Git-Custom-Show-Status
+
+
+# For git flow
+function Git-Custom-Flow-Feature {
+    git flow feature $args
+}
+function Git-Custom-Flow-Hotfix {
+    git flow hotfix $args
+}
+function Git-Custom-Flow-Init {
+    git flow init $args
+}
+function Git-Custom-Flow-Release {
+    git flow release $args
+}
+function Git-Custom-Flow-Support {
+    git flow support $args
+}
+function Git-Custom-Flow-Version {
+    git flow version $args
+}
+
+Set-Alias Gfeature Git-Custom-Flow-Feature
+Set-Alias Ghotfix Git-Custom-Flow-Hotfix
+Set-Alias Ginit Git-Custom-Flow-Init
+Set-Alias Grelease Git-Custom-Flow-Release
+Set-Alias Gsupport Git-Custom-Flow-Support
+Set-Alias Gversion Git-Custom-Flow-Version
