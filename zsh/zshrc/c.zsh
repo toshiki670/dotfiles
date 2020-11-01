2e4c55d7bdc4531e32313b296a4e5fc21de6d38e
Toshiki <toshiki.dev@protonmail.ch>
2020-11-01 23:10:22 +0900

    Change output file name 'feature/#62'

--- zsh/zshrc/c.zsh
+++ zsh/zshrc/c.zsh
@@ -1,9 +1,8 @@
 # 拡張子に応じたコマンドを実行
 function rungcc(){
-  gcc $1
-  base=$1
-  file=${base%.*}
-  ./a.out
-  rm -f a.out
+  output_file_name=$(md5sum $1 | awk '{print $2"."$1}')'.out'
+  gcc -o $output_file_name $1
+  ./$output_file_name
+  rm -f $output_file_name
 }
 alias -s {c,cpp}=rungcc
