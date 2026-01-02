b9b12da72bc70d8316590f0e470dabde53ce0355
Toshiki <bushy.trivial.0o@icloud.com>
2026-01-02 12:02:16 +0900

    refactor: extract yt-dlp config to external file

---
+++ zsh/zshrc/yt-dlp.zsh
@@ -0,0 +1,8 @@
+# yt-dlp config
+
+alias yt='yt-dlp --cookies-from-browser "chrome:Default"'
+# alias yt='yt-dlp --cookies-from-browser "firefox"'
+alias yt-comment='yt --write-subs --write-comments'
+alias yt-chat='yt --write-subs --write-comments'
+alias yt-vr='yt "https://www.nhk.or.jp/radio/ondemand/detail.html?p=6N87LJL8ZM_01"'
+
