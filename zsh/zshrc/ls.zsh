ba46657d9a974368a483aa673358e2267f912e01
Toshiki <bushy.trivial.0o@icloud.com>
2026-01-09 23:33:54 +0900

    Simplify eza aliases and add tree function
    - Remove commented dircolors configuration
    - Simplify ls/la aliases with icon support by default
    - Add lt() function for tree view with configurable depth
    - Remove redundant ll/lla aliases
    - Move sls alias outside conditional block

--- zsh/zshrc/ls.zsh
+++ zsh/zshrc/ls.zsh
@@ -1,32 +1,16 @@
-# # Theme configure
-# # Install location
-# DIRCOLORS=${DOTFILES}/zsh/bundle/dircolors-solarized
-
-# # Dircolors installation
-# if $(type "git" > /dev/null 2>&1) && [[ ! -d $DIRCOLORS ]]; then
-#   git clone https://github.com/seebi/dircolors-solarized.git $DIRCOLORS
-# fi
-
-# # Dircolors activation
-# if [[ -d $DIRCOLORS ]]; then
-#   eval $(dircolors $DIRCOLORS)
-#   eval $(dircolors $DIRCOLORS/dircolors.ansi-universal)
-# fi
-
-
 # ls command series
 if type "eza" > /dev/null 2>&1; then
-  # pacman -S eza
-  alias ls='eza'
-  alias ll='eza  -l  -gh --time-style long-iso --git'
-  alias la='eza  -a'
-  alias lla='eza -la -gh --time-style long-iso --git'
+  alias ls='eza --icons=always -l -gh --time-style long-iso --git'
+  alias la='eza --icons=always -la -gh --time-style long-iso --git'
 
-  # Standard ls (always use standard ls command)
-  alias sls='command ls --color=auto'
+  lt() {
+    local level="${1:-2}"  # デフォルトは2
+    eza --icons=always -la -gh --time-style long-iso --git --tree --level="$level"
+  }
 else
-  alias ls='ls  --color=auto'
-  alias ll='ls  -lh'
-  alias la='ls  -a'
-  alias lla='ls -lah'
+  alias ls='ls --color=auto -lh'
+  alias la='ls --color=auto -lah'
 fi
+
+# Standard ls (always use standard ls command)
+alias sls='command ls'
