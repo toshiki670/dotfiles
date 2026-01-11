69fe6c614b36258a4ca888b3a4fa6c100ef4d53b
toshiki670 <bushy.trivial.0o@icloud.com>
2026-01-11 12:55:32 +0900

    Remove unused yt-dlp aliases
    Remove yt-comment and yt-vr aliases as they are no longer needed.
    Keep yt-chat alias which serves the same purpose as yt-comment.

--- zsh/zshrc/yt-dlp.zsh
+++ zsh/zshrc/yt-dlp.zsh
@@ -6,7 +6,4 @@
 YT_BROWSER="${YT_BROWSER:-chrome:Default}"
 
 alias yt="yt-dlp --cookies-from-browser \"${YT_BROWSER}\""
-alias yt-comment='yt --write-subs --write-comments'
 alias yt-chat='yt --write-subs --write-comments'
-alias yt-vr='yt "https://www.nhk.or.jp/radio/ondemand/detail.html?p=6N87LJL8ZM_01"'
-
