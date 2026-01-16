ce065f987a61b7b4ef589aee4dd2973bfadfe78e
toshiki670 <bushy.trivial.0o@icloud.com>
2026-01-17 01:14:04 +0900

    fix: use dirname to extract parent directory from completion file paths
    - Changed from hardcoded completions directory to extracting parent from variables
    - More flexible approach that works regardless of comp_docker/comp_docker_compose values

--- dot_config/zsh/configs/docker.zsh
+++ dot_config/zsh/configs/docker.zsh
@@ -5,14 +5,14 @@ completions=${DOTFILES}/zsh/completions
 # For docker
 comp_docker=${completions}/_docker
 if [[ ! -e ${comp_docker} ]] && type "docker" > /dev/null 2>&1; then
-  mkdir -p ${completions}
+  mkdir -p "$(dirname "${comp_docker}")"
   curl -L https://raw.githubusercontent.com/docker/cli/master/contrib/completion/zsh/_docker > ${comp_docker}
 fi
 
 # For docker-compose
 comp_docker_compose=${completions}/_docker-compose
 if [[ ! -e ${comp_docker_compose} ]] && type "docker-compose" > /dev/null 2>&1; then
-  mkdir -p ${completions}
+  mkdir -p "$(dirname "${comp_docker_compose}")"
   curl -L https://raw.githubusercontent.com/docker/compose/$(docker-compose version --short)/contrib/completion/zsh/_docker-compose > ${comp_docker_compose}
 fi
 
