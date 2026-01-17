0727c4483a2e1d4c0e285377343861cfbc0d3003
toshiki670 <bushy.trivial.0o@icloud.com>
2026-01-17 19:14:51 +0900

    Add conditional loading for yt-dlp config
    - Only include yt-dlp config if YT_BROWSER environment variable is set
    - Remove default YT_BROWSER value (chrome:Default)
    - More flexible configuration without forcing defaults
    
    Benefits:
    - Cleaner config when yt-dlp is not used
    - No unnecessary aliases loaded
    - User must explicitly set YT_BROWSER to enable yt-dlp integration

--- dot_config/zsh/configs/yt-dlp.zsh
+++ dot_config/zsh/configs/yt-dlp.zsh
@@ -2,8 +2,5 @@
 
 # Browser selection via environment variable
 # Set YT_BROWSER in ~/.config/mise/config.toml or export in shell
-# Default: chrome:Default
-YT_BROWSER="${YT_BROWSER:-chrome:Default}"
-
 alias yt="yt-dlp --cookies-from-browser \"${YT_BROWSER}\""
 alias yt-chat='yt --write-subs --write-comments'
